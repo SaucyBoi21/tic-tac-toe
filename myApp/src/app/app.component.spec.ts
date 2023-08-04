35d66779247bb1d35bde1eee9b7044be68fd5f1a
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-08-03 22:38:19 -0400

    fixed some imports

--- myApp/src/app/app.component.spec.ts
+++ myApp/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
