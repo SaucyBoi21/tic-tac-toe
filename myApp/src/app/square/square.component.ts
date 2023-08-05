690208e9dd41e6cd621ed25fa2b55da0890afec9
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-08-04 22:43:02 -0400

    added component

--- myApp/src/app/square/square.component.ts
+++ myApp/src/app/square/square.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-square',
-  standalone: true,
-  imports: [CommonModule],
   template: `
     <p>
       square works!
     </p>
   `,
-  styleUrls: ['./square.component.scss']
+  styles: [
+  ]
 })
 export class SquareComponent {
 
